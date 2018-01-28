17ce76c314f2d3f6a6db30f56d850afd6d21d846
boyaxxx <xiaolu625@gmail.com>
2018-01-28 22:32:25 +0800

    添加注释
    添加注释 01.28

--- analysis/views.py
+++ analysis/views.py
@@ -8,27 +8,27 @@ from .backend_analysis import user_in_news_analysis
 def index(request):
     return render(request, 'frontend/index.html')
 
-
+#计算文章热度
 def compute_news_hot(request):
     rst = content_analysis.compute_news_hot()
     return HttpResponse(rst)
 
-
+#计算用户热度
 def compute_users_hot(request):
     rst = user_analysis.compute_users_hot()
     return HttpResponse(rst)
 
-
+#生成转发路径树
 def get_transmit_tree(request):
     rst = user_in_news_analysis.get_transmit_tree(6)
     return HttpResponse(rst)
 
-
+#发现转发重点用户
 def find_important_user(request):
     rst = user_in_news_analysis.find_important_user_django(6)
     return HttpResponse(rst)
 
-
+#发现转发重点路径
 def find_important_path(request):
     rst = user_in_news_analysis.find_important_path(6)
     return HttpResponse(rst)
