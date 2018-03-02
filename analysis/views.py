101d1947252f1e588c061fdcafcde75a8e0e48b1
boyaxxx <xiaolu625@gmail.com>
2018-03-02 10:22:46 +0800

    更新分页接口
    增加总数

--- analysis/views.py
+++ analysis/views.py
@@ -53,11 +53,13 @@ def get_latest_news(request, top=3):
 
 #最新内容分页
 def get_latest_news_by_page(request, page_number=1, page_size=10):
+    rst = {}
     rst_list = News.objects.all().order_by("-createdAt").values("title", "writerName", "introduction","newsId","createdAt")
+    rst['total_count'] = len(rst_list)
     if len(rst_list) >= page_size:
         rst_list = rst_list[page_size*(page_number-1):page_size*page_number]
-    rst = json.dumps(list(rst_list), cls=DjangoJSONEncoder)
-    # rst = serializers.serialize("json",rst_list)
+    rst['content_list'] = list(rst_list)
+    rst = json.dumps(rst, cls=DjangoJSONEncoder)
     return HttpResponse(rst)
 
 #最新活跃用户
@@ -71,10 +73,13 @@ def get_latest_users(request, top=3):
 
 #最新活跃用户分页
 def get_latest_users_by_page(request, page_number=1, page_size=10):
+    rst = {}
     rst_list = TransmitNews.objects.all().order_by("-updatedAt").values("viewerId","viewerName","viewerHeadImg", "updatedAt")
+    rst['total_count'] = len(rst_list)
     if len(rst_list) >= page_size:
         rst_list = rst_list[page_size*(page_number-1):page_size*page_number]
-    rst = json.dumps(list(rst_list), cls=DjangoJSONEncoder)
+    rst['user_list'] = list(rst_list)
+    rst = json.dumps(rst, cls=DjangoJSONEncoder)
     return HttpResponse(rst)
 
 # 用户行为日志
